3e2e826e9cf9ca8a1f4d6e97c62e292ee059fa0f
Jose Leonardo Gonzalez Valadez <jleonardo18071999@gmail.com>
2025-12-15 12:13:56 -0600

    add migraciones

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\ResetPasswordController;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,3 +14,10 @@ Route::post('password/update', [ResetPasswordController::class, 'reset'])->name(
 Route::get('password/reset/success', function () {
     return '¡Tu contraseña ha sido restablecida con éxito!';
 })->name('password.reset.success');
+
+
+Route::get('/deploy-migrate', function () {
+    Artisan::call('migrate --force');
+    Artisan::call('db:seed --force');
+    return 'Migraciones y seeders ejecutados correctamente';
+});
